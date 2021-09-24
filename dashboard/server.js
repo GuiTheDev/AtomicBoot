6437f481aefcc4a5c14917bd0b96f59ff2f1145f
ZEdasmoscas <alexandreguilhermemx@gmail.com>
2021-09-24 21:03:13 +0100

    more of the website :)

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,4 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
  
-app.listen(3000, () => console.log('web server ON on port 3000'))
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => res.render('index', {
+    something: 'testing pog'
+}));
+
+app.listen(3000, () => console.log('web server ON on port 3000'));
