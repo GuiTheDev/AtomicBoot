f7dd1439b45bfb7db949592f5980caf5148a1c00
ZEdasmoscas <alexandreguilhermemx@gmail.com>
2021-09-24 23:48:25 +0100

    homepage is done and commands page is +half done :) uff...

--- dashboard/server.js
+++ dashboard/server.js
@@ -4,10 +4,18 @@ const app = express();
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug');
+app.use(express.static(`${__dirname}/assets`));
+app.locals.basedir = `${__dirname}/assets`;
 
 
-app.get('/', (req, res) => res.render('index', {
-    something: 'testing pog'
+app.get('/', (req, res) => res.render('index'));
+app.get('/commands', (req, res) => res.render('commands', { 
+    subtitle: 'Commands',
+    categories: [
+        { name: 'Auto Mod', icon: 'fas fa-gavel' },
+        { name: 'Economy', icon: 'fas fa-coins' },
+        { name: 'General', icon: 'fas fa-star' },
+        { name: 'Music', icon: 'fas fa-music'}],
 }));
 
 app.listen(3000, () => console.log('web server ON on port 3000'));
