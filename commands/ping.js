2b8ad149b770d2b048d5c3e4c77041ac4328fa72
ZEdasmoscas <alexandreguilhermemx@gmail.com>
2021-09-30 09:18:55 +0100

    POg

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,6 @@ module.exports = {
     name : 'ping',
     description: "Ping Command!",
     execute(client, message, args, Discord) {
-        if(!profileData) return message.reply('Looks like this is your first time, you need to run the command again!');
         console.log("Started command")
         console.log('Executed');
         message.channel.send(`🏓 Pong! Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
